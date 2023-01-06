b690f7e1d4cf156b523c2837c70d70d5bec7a32a
Francisco Javier Sanchez Temprano <frango9000@gmail.com>
2023-01-06 17:08:14 +0100

    base ui

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,37 @@
 import com.soywiz.korge.gradle.*
 
-plugins {
-	alias(libs.plugins.korge)
+buildscript {
+    repositories {
+        mavenLocal()
+        google()
+        maven { url = uri("https://dl.bintray.com/korlibs/korlibs") }
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+        mavenCentral()
+        google()
+    }
+    dependencies {
+        classpath("com.soywiz.korlibs.korge.plugins:korge-gradle-plugin:3.4.0")
+    }
 }
 
+apply<KorgeGradlePlugin>()
+
 korge {
-	id = "dev.kurama.puzzle2048"
+    id = "dev.kurama.puzzle2048"
+    name = "2048"
+    icon = file("src/commonMain/resources/korge.png")
 // To enable all targets at once
 
-	//targetAll()
+    //targetAll()
 
 // To enable targets based on properties/environment variables
-	//targetDefault()
+    //targetDefault()
 
 // To selectively enable targets
 
-	targetJvm()
-	targetJs()
-	//targetDesktop()
-	//targetIos()
-	//targetAndroidIndirect() // targetAndroidDirect()
+    targetJvm()
+    targetJs()
+    //targetDesktop()
+    //targetIos()
+    //targetAndroidIndirect() // targetAndroidDirect()
 }
--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,35 +1,93 @@
-import com.soywiz.klock.*
 import com.soywiz.korge.*
-import com.soywiz.korge.scene.*
-import com.soywiz.korge.tween.*
 import com.soywiz.korge.view.*
 import com.soywiz.korim.color.*
+import com.soywiz.korim.font.*
 import com.soywiz.korim.format.*
+import com.soywiz.korim.text.*
 import com.soywiz.korio.file.std.*
 import com.soywiz.korma.geom.*
-import com.soywiz.korma.interpolation.*
+import com.soywiz.korma.geom.vector.*
 
-suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"]) {
-	val sceneContainer = sceneContainer()
 
-	sceneContainer.changeTo({ MyScene() })
-}
+suspend fun main() = Korge(width = 480, height = 640, title = "2048", bgcolor = RGBA(253, 247, 240)) {
+    val cellSize = views.virtualWidth / 5.0
+    val fieldSize = 50 + 4 * cellSize
+    val leftIndent = (views.virtualWidth - fieldSize) / 2
+    val topIndent = 150.0
 
-class MyScene : Scene() {
-	override suspend fun SContainer.sceneMain() {
-		val minDegrees = (-16).degrees
-		val maxDegrees = (+16).degrees
+    val bgField = roundRect(fieldSize, fieldSize, 5.0, fill = Colors["#b9aeaf"]) {
+        position(leftIndent, topIndent)
+    }
 
-		val image = image(resourcesVfs["korge.png"].readBitmap()) {
-			rotation = maxDegrees
-			anchor(.5, .5)
-			scale(0.8)
-			position(256, 256)
-		}
+    graphics {
+        it.position(leftIndent, topIndent)
+        fill(Colors["#cdc0b2"]) {
+            for (i in 0..3) {
+                for (j in 0..3) {
+                    roundRect(10 + (10 + cellSize) * i, 10 + (10 + cellSize) * j, cellSize, cellSize, 5.0)
+                }
+            }
+        }
+    }
 
-		while (true) {
-			image.tween(image::rotation[minDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
-			image.tween(image::rotation[maxDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
-		}
-	}
+    val bgLogo = roundRect(cellSize, cellSize, 5.0, fill = Colors["#edc403"]) {
+        position(leftIndent, 30.0)
+    }
+
+    val bgBest = roundRect(cellSize * 1.5, cellSize * 0.8, 5.0, fill = Colors["#bbae9e"]) {
+        alignRightToRightOf(bgField)
+        alignTopToTopOf(bgLogo)
+    }
+
+    val bgScore = roundRect(cellSize * 1.5, cellSize * 0.8, 5.0, fill = Colors["#bbae9e"]) {
+        alignRightToLeftOf(bgBest, 24)
+        alignTopToTopOf(bgBest)
+    }
+
+    val font = resourcesVfs["clear_sans.fnt"].readBitmapFont()
+    text("2048", cellSize * 0.5, Colors.WHITE, font).centerOn(bgLogo)
+
+    text("BEST", cellSize * 0.25, RGBA(239, 226, 210), font) {
+        centerXOn(bgBest)
+        alignTopToTopOf(bgBest, 5.0)
+    }
+    text("0", cellSize * 0.5, Colors.WHITE, font) {
+        setTextBounds(Rectangle(0.0, 0.0, bgBest.width, cellSize - 24.0))
+        alignment = TextAlignment.MIDDLE_CENTER
+        alignTopToTopOf(bgBest, 12.0)
+        centerXOn(bgBest)
+    }
+    text("SCORE", cellSize * 0.25, RGBA(239, 226, 210), font) {
+        centerXOn(bgScore)
+        alignTopToTopOf(bgScore, 5.0)
+    }
+    text("0", cellSize * 0.5, Colors.WHITE, font) {
+        setTextBounds(Rectangle(0.0, 0.0, bgScore.width, cellSize - 24.0))
+        alignment = TextAlignment.MIDDLE_CENTER
+        centerXOn(bgScore)
+        alignTopToTopOf(bgScore, 12.0)
+    }
+
+    val restartImg = resourcesVfs["restart.png"].readBitmap()
+    val undoImg = resourcesVfs["undo.png"].readBitmap()
+
+    val btnSize = cellSize * 0.3
+    val restartBlock = container {
+        val background = roundRect(btnSize, btnSize, 5.0, fill = RGBA(185, 174, 160))
+        image(restartImg) {
+            size(btnSize * 0.8, btnSize * 0.8)
+            centerOn(background)
+        }
+        alignTopToBottomOf(bgBest, 5)
+        alignRightToRightOf(bgField)
+    }
+    val undoBlock = container {
+        val background = roundRect(btnSize, btnSize, 5.0, fill = RGBA(185, 174, 160))
+        image(undoImg) {
+            size(btnSize * 0.6, btnSize * 0.6)
+            centerOn(background)
+        }
+        alignTopToTopOf(restartBlock)
+        alignRightToLeftOf(restartBlock, 5.0)
+    }
 }
